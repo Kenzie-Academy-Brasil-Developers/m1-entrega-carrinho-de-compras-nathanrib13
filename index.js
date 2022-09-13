3e9b55e35e8035c9f8d532cf26dab99def76f2e0
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2022-09-13 12:15:26 -0300

    enviando tarefa

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-const productsCart = [
-    {
-        id: 1,
-        name: "",
-        price: 0.00
-    }
-]
+
+
+let body               = document.querySelector("body")
+let main               = document.createElement("main")
+let ul                 = document.createElement("ul")
+let section          = document.createElement("section")
+let button            = document.createElement("button")
+let divInicio        = document.createElement("div")
+let descriptionItem = document.createElement("p")
+let descriptionPrice = document.createElement("p")
+let divFinal            = document.createElement("div")
+let totalCompra         = document.createElement("p")
+let totalCompraValor    = document.createElement("p")
+let h1                    = document.createElement ("h1")
+
+body.appendChild(h1)
+body.appendChild(main)
+main.appendChild(divInicio)
+divInicio.appendChild(descriptionItem)
+divInicio.appendChild(descriptionPrice)
+main.appendChild(ul)
+main.appendChild(section)
+section.appendChild(divFinal)
+divFinal.appendChild(totalCompra)
+divFinal.appendChild(totalCompraValor)
+section.appendChild(button)
+
+
+let valorTotal = 0
+
+for(let i = 0; i<productsCart.length; i++){
+    let li = document.createElement("li")
+    ul.appendChild(li)
+    let pProduct = document.createElement("p")
+    let pPrice = document.createElement("p")
+    pProduct.innerHTML += `${productsCart[i].name}` 
+    pPrice.innerHTML += ` R$ ${productsCart[i].price}`
+    li.appendChild(pProduct)
+    li.appendChild(pPrice)
+    valorTotal   +=   productsCart[i].price
+}
+
+h1.innerHTML        = 'Virtual Market'
+totalCompra.innerHTML = `Total `
+totalCompraValor.innerHTML= `R$ ${valorTotal}`
+button.innerHTML      = "Finalizar Compra"
+descriptionItem.innerHTML = `Item` 
+descriptionPrice.innerHTML = `Valor`
+
---
+++ products.js
@@ -0,0 +1,33 @@
+const productsCart = [
+    {
+        id: 1,
+        name: "Salame Seara gourmet",
+        price: 36.43
+    },
+    {
+        id: 2,
+        name: "Coca-Cola light",
+        price: 8.99
+    },
+    {
+        id: 3,
+        name: "Manjericão Fresco",
+        price: 3.00
+    },
+    {
+        id: 4,
+        name: "Pão italiano",
+        price: 12.00
+    },
+    { 
+        id: 5,
+        name: "Mostarda Dijon",
+        price: 28.50
+    },
+    {
+        id: 6,
+        name: "Queijo Gouda",
+        price: 19.70
+    },
+    
+]
